9c72795f9de8274c2d8fd23cb3d9f06d028c9f37
chiragkatare <chiragkatare123@gmail.com>
2018-10-05 19:14:01 +0530

    evening last commit  05/10/2018

---
+++ Functional/src/FlipCoin.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class FlipCoin {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	System.out.println("enter  no of times  to flip the coin");
+	int times = s.nextInt();
+	
+}
+}
